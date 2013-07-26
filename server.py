5ad7a8e5f44f57d87e8aaf044f9c3ed78fb274b1
Ed Hunsinger <edrabbit@edrabbit.com>
2013-07-26 01:40:15 -0700

    Added switch for easier handling of different receivers. Improved logging functionality. Please use it! Easier to debug, and we can pull of some fun statistics later.

--- server.py
+++ server.py
@@ -5,49 +5,88 @@ fadecandy (https://github.com/scanlime/fadecandy)
 
 @author: Ed, Samson, April
 
+
+Please use logging in key="value" format for statistics and debugging. Ed wants
+to Splunk the cloud.
+
 """
-import liblo
-import sys
 import logging
+import sys
+import time
 
-logging.basicConfig(level=logging.DEBUG)
+import liblo
+
+CONSOLE_LOG_LEVEL = logging.DEBUG
+FILE_LOG_LEVEL = logging.DEBUG
+LOG_FILE = 'amcpserver.log'
 
 
-class MyServer(liblo.ServerThread):
-    def __init__(self):
-        liblo.ServerThread.__init__(self, 1234)
+# Setup all our logging. Timestamps will be in localtime.
+# TODO(ed): Figure out how to get the timezone offset in the log, or use UTC
+logger = logging.getLogger('amcpserver')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('amcpserver.log')
+fh.setLevel(FILE_LOG_LEVEL)
+ch = logging.StreamHandler()
+ch.setLevel(CONSOLE_LOG_LEVEL)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+class AMCPServer(liblo.ServerThread):
+    def __init__(self, port):
+        logger.debug('action="init_server", port="%s"' % port)
+        liblo.ServerThread.__init__(self, port)
 
     @liblo.make_method(None, None)
-    def catchall(self, path, args):
-        path = path.split("/")
-        logging.debug("%s, %s" % (path, args))
-        if path[2] == 'toggle1':
-            Water().all_the_rain()
+    def catch_all(self, path, args):
+        logger.debug('action="catch_all", path="%s", args="%s"' % (path, args))
+        p = path.split("/")
+        switch = {
+            'water_rain': Water().rain,
+            'water_make_it_rain': Water().make_it_rain,
+            'sound_thunder': SoundEffects().thunder,
+            'sound_rain': SoundEffects().rain,
+            'sound_its_raining_men': SoundEffects().its_raining_men,
+            'light_lightning': Lighting().strobe
+        }
+        try:
+            switch[p[2]](*args)
+        except KeyError:
+            logger.error(
+                'action="catch_all", path="%s", error="not found" args="%s"'
+                % (path, args))
 
     def whitepoint(self, path, args):
         value = args[0]
         path = path.split("/")
         print path[3]
+        logger.debug('action="set_whitepoint", channel="%s", value="%s"'
+                     % (path[3], args[0]))
         if path[3] == '1':
-            logging.debug("Changing R values to %s" % value)
             # TODO: Send R changes to fadecandy
+            pass
         elif path[3] == '2':
-            logging.debug("Changing G values to %s" % value)
             # TODO: Send G changes to fadecandy
+            pass
         elif path[3] == '3':
-            logging.debug("Changing B values to %s" % value)
             # TODO: Send B changes to fadecandy
+            pass
 
     @liblo.make_method(None, 'f')
     def gamma(self, path, args):
         value = args[0]
 
 
-    @liblo.make_method('/foo', 'ifs')
-    def foo_callback(self, path, args):
-        i, f, s = args
-        logging.debug("received message '%s' with arguments: %d, %f, %s"
-                     % (path, i, f, s))
+    # @liblo.make_method('/foo', 'ifs')
+    # def foo_callback(self, path, args):
+    #     i, f, s = args
+    #     logger.debug("received message '%s' with arguments: %d, %f, %s"
+    #                  % (path, i, f, s))
 
     # @liblo.make_method(None, None)
     # def fallback(self, path, args):
@@ -56,9 +95,24 @@ class MyServer(liblo.ServerThread):
 
 class Water():
     """Controls rain, mist, etc"""
+
+    def rain(self, toggle):
+        logger.debug('action="rain", toggle="%s"' % toggle)
+        if toggle:
+            logger.info('action="rain", toggle="on"')
+        else:
+            logger.info('action="rain", toggle="off"')
+
+    def make_it_rain(self):
+        logger.info('action="make_it_rain"')
+        self.rain(True)
+        time.sleep(5000)
+        self.rain(False)
+        pass
+
     def all_the_rain(self):
         # PiGPIO.send(1,1)
-        logging.debug("All the rain")
+        logger.info('action="all_the_rain"')
         pass
 
     def rain_timer(self, length):
@@ -73,6 +127,8 @@ class PiGPIO():
 
     def send(self, pin_num, value):
         """Send value (1/0) to pin_num"""
+        logger.debug('action="send_rpi_gpio", pin_number="%i", value="%i"'
+                     % (pin_num, value))
         pass
 
 
@@ -95,22 +151,28 @@ class OPC():
         pass
 
 
-class SoundEffect():
+class SoundEffects():
     """Play different sound effects.
 
     Probably want to index these sounds somehow? Config file?"""
 
-    def thunder(self):
-        #SoundOut.play(thunderfile.wav)
-        pass
+    def thunder(self, press):
+        sound_file = 'thunder.wav'
+        if press:
+            #SoundOut.play(sound_file)
+            logger.info('action="play_sound", soundfile="%s"' % (sound_file))
 
-    def rain(self):
-        #SoundOut.play(rainsounds.wav)
-        pass
+    def rain(self, press):
+        sound_file = 'rainsounds.wav'
+        if press:
+            #SoundOut.play(sound_file)
+            logger.info('action="play_sound", soundfile="%s"' % (sound_file))
 
-    def its_raining_men(self):
-        #SoundOut.play(its_raining_men.wav)
-        pass
+    def its_raining_men(self, press):
+        sound_file = 'its_raining_men.wav'
+        if press:
+            #SoundOut.play(sound_file)
+            logger.info('action="play_sound", soundfile="%s"' % (sound_file))
 
 
 class SoundOut():
@@ -123,10 +185,12 @@ class SoundOut():
 
 if (__name__ == "__main__"):
     try:
-        server = MyServer()
-    except ServerError, err:
+        server = AMCPServer(1234)
+    except liblo.ServerError, err:
         print str(err)
         sys.exit()
 
     server.start()
+    # TODO(ed): Pausing for input seems like a janky way to run a server
     raw_input("press enter to quit...\n")
+    logger.debug('action="server_shutdown"')
