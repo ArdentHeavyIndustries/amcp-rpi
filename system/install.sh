be61f00683ed8ded9f0703f1bfe8fb35dc5f78de
Samson Yeung <samson.yeung@oracle.com>
2013-08-12 22:56:36 -0700

    system/install.sh: Update to apt-get deps.

--- system/install.sh
+++ system/install.sh
@@ -3,8 +3,17 @@
 # Install system configuration and pre-built binaries to
 # a Raspbian system.
 #
+# the intention is for the setup procedure to be:
+#   1) Install Raspbian
+#   2) Clone git@github.com:ArdentHeavyIndustries/amcp-rpi.git into /home/pi/amcp-rpi.git
+#   3) Run this install.sh
+#   4) Reboot
 
-apt-get install python-avahi
+sudo apt-get install python-avahi python-dev supervisor avahi-daemon python-liblo libao4
+
+python ../server.py build --build-platlib=.
+
+sudo cp supervisor/amcp.conf /etc/supervisor/conf.d/
 
 sudo cp -r etc usr lib /
 sudo update-rc.d fcserver defaults
